03c0ea5988672dc2e64c1f1845656b43cf60d7e3
kranthi <kumar_android@outlook.com>
2017-04-07 09:36:59 -0400

    removed junk

--- app/src/main/java/com/example/kranthikumarpolimetla/dagger2practice/MainActivity.java
+++ app/src/main/java/com/example/kranthikumarpolimetla/dagger2practice/MainActivity.java
@@ -37,17 +37,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         AddNums.getInstance().getAddNumsComponent().inject(this);
-/*num1 = (EditText) findViewById(R.id.number1);
-        num2 = (EditText) findViewById(R.id.number2);
-        add = (Button) findViewById(R.id.button);
-        output = (TextView) findViewById(R.id.result);*/
     }
 
     public void addClicked(View view) {
         numb1 = Integer.parseInt(num1.getText().toString());
         numb2 = Integer.parseInt(num2.getText().toString());
         output.setText(addNums.adding(numb1, numb2)+"");
-
-
     }
 }
